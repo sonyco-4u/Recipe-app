904e1014b20f90176a1f33c4ee31e177897784f7
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-14 23:55:58 +0300

    Create controllers for the appliction

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,13 @@
+class FoodsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,13 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+end
