737e7d66cc13cc41f57910f59d4f71506e12b074
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-02-14 08:05:51 -0800

    Add association for recipe and recipefood model

--- app/models/recipefood.rb
+++ app/models/recipefood.rb
@@ -1,3 +1,5 @@
 class Recipefood < ApplicationRecord
   belongs_to :food, foreign_key: :food_id
+  belongs_to :recipe, foreign_key: :recipe_id
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :foods
+  has_many :recipes
 end
+
