7cdaaf898bcc1ccf3368c1a7062766cfb06f1a75
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-15 00:01:40 +0300

    Create test for controllers

---
+++ test/controllers/recipes_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class RecipesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get recipes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get recipes_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get recipes_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get recipes_new_url
+    assert_response :success
+  end
+end
