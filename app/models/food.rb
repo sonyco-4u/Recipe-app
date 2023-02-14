e1a25ef52f3697e516a67ed2665734eae5798e18
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-14 18:33:14 +0300

    Create association for user, food and recipefood models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,4 @@
 class Food < ApplicationRecord
+  has_many :recipe_foods
+  belongs_to :user, foreign_key: :user_id
 end
--- app/models/recipefood.rb
+++ app/models/recipefood.rb
@@ -1,2 +1,3 @@
 class Recipefood < ApplicationRecord
+  belongs_to :food, foreign_key: :food_id
 end
