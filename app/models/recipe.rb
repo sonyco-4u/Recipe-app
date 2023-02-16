c90611a3d31f933361486e9aa2f5b6a06da65c16
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-16 19:35:28 +0300

    Add a method to show list of public foods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   has_many :recipe_foods, dependent: :destroy
+
+  def list_all_public_recipes
+    public_recipes.order('created_at ASC')
+  end
+
+  def total_food_items
+    food.size
+  end
 end
