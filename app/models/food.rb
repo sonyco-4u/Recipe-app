c8dc99b2b43dbe50e48ba0bab1927766c31d7f1f
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-16 16:48:37 +0300

    Create models, views and controllers

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   belongs_to :user, foreign_key: :user_id
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
 end
