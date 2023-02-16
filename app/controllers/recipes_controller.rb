f0a785dd3fefa0849e5477a9a1d98fbf45816e27
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-17 00:43:14 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,12 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
-
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
   end
 
   # GET /recipes/1 or /recipes/1.json
+
   def show
     @user = User.find(params[:id])
     @recipe = @user.recipes.find(params[:id])
