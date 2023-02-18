a32480671e1f4b868ee8ddf7836c2061d579158d
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-18 11:00:51 +0300

    Remove a few files

--- app/controllers/recipefoods_controller.rb
+++
@@ -1,7 +0,0 @@
-class RecipefoodsController < ApplicationController
-  before_action :set_recipefood, only: %i[show edit update destroy]
-  # GET /recipefoods or /recipefoods.json
-  def index
-    @recipefoods = Recipefood.all
-  end
-end
