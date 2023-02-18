d56d704ad2a0625e32ae03d596968fc7922b165d
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-18 10:54:47 +0300

    Add validations to the models

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,6 @@
 class RecipeFood < ApplicationRecord
   belongs_to :food, foreign_key: :food_id
   belongs_to :recipe, foreign_key: :recipe_id
+
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
